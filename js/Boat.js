06f06f47346c21c125360b66b276c6a5ba21e17e
R1sh33n <96629065+R1sh33n@users.noreply.github.com>
2022-03-19 16:46:10 +0000

    Add files via upload

---
+++ js/Boat.js
@@ -0,0 +1,44 @@
+class Boat {
+  constructor(x, y, width, height, boatPos, boatAnimation) {
+  
+    this.body = Bodies.rectangle(x, y, width, height);
+    this.animation = boatAnimation;
+    this.width = width;
+    this.height = height;
+    this.speed = 0.05;
+
+    this.boatPosition = boatPos;
+    this.isBroken = false;
+
+    World.add(world, this.body);
+  }
+
+  animate(){
+    this.speed +=0.5;
+  }
+
+  remove(index) {
+    this.animation = brokenBoatAnimation;
+    this.speed = 0.05;
+    this.width = 300;
+    this.height = 300;
+    this.isBroken = true;
+    setTimeout(() => {
+      Matter.World.remove(world, boats[index].body);
+      delete boats[index];
+    }, 2000);
+  }
+
+  display() {
+    var angle = this.body.angle;
+    var pos = this.body.position;
+    var index = floor(this.speed % this.animation.length);
+
+    push();
+    translate(pos.x, pos.y);
+    rotate(angle);
+    imageMode(CENTER);
+    image(this.animation[index], 0, this.boatPosition, this.width, this.height);
+    pop();
+  }
+}
